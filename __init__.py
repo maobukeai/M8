b97fb97f5bf188b805c739c43786640b6bde189a
maobukeai <maobukeai.lilan@gmail.com>
2026-03-19 20:50:05 +0800

    bump version to 3.1.0

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "M8全能工具箱",
     "author": "猫步可爱",
-    "version": (3, 0, 1),
+    "version": (3, 1, 0),
     "blender": (3, 0, 0),
     "location": "View3D > Sidebar (N) > M8 Tool",
     "description": "在复杂的 3D 建模项目中，效率就是生命。M8 不仅仅是一个工具箱，它是你工作流中的润滑剂。",
